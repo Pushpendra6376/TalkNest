633cc5bb24e23d9f0ad1e78eac482c9700c4146b
Pushpendra6376 <pp5395021@gmail.com>
2026-03-02 22:07:12 +0530

    make auth apis

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import User from "../models/user.model";
+
+export const register = (req, res) => {
+
+}
+
+export const login = (req, res) => {
+    
+}
+
+export const logout = (req, res) => {
+    
+}
+
+export default {register, login, logout};
